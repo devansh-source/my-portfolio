ca8d3205a4932d4a164f5391e8823e8d3b0d2d08
devanshpanchal123-pixel <devanshpanchal123@gmail.com>
2026-04-19 19:38:34 +0530

    fix: disable 3d rotation on mobile to fix page scrolling

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -67,6 +67,7 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          enableRotate={!isMobile}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
